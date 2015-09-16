633acb8b58d6fbcaecfe9fe90a0ea73724d053c9
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-09-16 15:23:14 +1000

    minor refactor

--- client/src/ducks/counter.js
+++ client/src/ducks/counter.js
@@ -45,10 +45,13 @@ export function incrementAsync(delay = 1000) {
 // REDUCER
 export function reducer(state = initialState, action) {
   switch (action.type) {
+
     case INCREMENT:
       return {...state, count: state.count + action.amount }
+
     case DECREMENT:
       return {...state, count: state.count - action.amount }
+      
     default:
       return state
   }
--- client/src/ducks/index.js
+++ client/src/ducks/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import * as clients from './clients'
 import * as counter from './counter'
 
-// export the root reducer
+// export the root reducer, these become the top level of the global tree
 export default combineReducers(
   {
     clients: clients.reducer,
