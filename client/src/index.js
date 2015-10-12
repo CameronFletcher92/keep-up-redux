af34ed50b5854ebc20b5cb163dac1a5cb4fd8705
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-10-12 20:21:38 +1000

    reverted to react 0.13 to attempt the switch to material ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+//import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import Root from './smart/Root'
 
 // render the root
-ReactDOM.render(<Root />, document.getElementById('root'))
+React.render(<Root />, document.getElementById('root'))
