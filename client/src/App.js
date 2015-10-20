5b24dab99fa36041550ba2831567dc63c44cf024
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-10-20 18:48:59 +1000

    further hidden devtools

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,19 @@ import { ReduxRouter } from 'redux-router'
 import shouldUpdatePure from 'react-pure-render/function'
 import store from './store'
 import routes from './routes'
-import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react'
+//import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react'
 
 class App extends Component {
   shouldComponentUpdate = shouldUpdatePure
 
   renderDevTools() {
+    /*
     return (
       <DebugPanel top right bottom>
         <DevTools store={store} monitor={LogMonitor} />
       </DebugPanel>
     )
+    */
   }
 
   render() {
