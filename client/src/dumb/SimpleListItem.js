93c4d389dfc9c1dfd2dd4bfe6b07ae215adaad96
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-10-21 17:23:42 +1000

    margin on the item busy indicator

--- client/src/dumb/SimpleListItem.js
+++ client/src/dumb/SimpleListItem.js
@@ -5,6 +5,14 @@ import { ListItem, Avatar, Styles, LinearProgress } from 'material-ui'
 class SimpleListItem extends Component {
   shouldComponentUpdate = shouldUpdatePure
 
+  renderProgress() {
+    return (
+      <div style={{ padding: '1em' }}>
+        <LinearProgress mode='indeterminate'/>
+      </div>
+    )
+  }
+
   render() {
     const { name, editClicked, busy, letter } = this.props
 
@@ -13,7 +21,7 @@ class SimpleListItem extends Component {
         <ListItem primaryText={name}
                   leftAvatar={letter ? <Avatar color={Styles.Colors.pinkA200} backgroundColor={Styles.Colors.cyanA100}>{letter}</Avatar> : null}
                   onClick={editClicked} disabled={busy} insetChildren={true}/>
-        {busy ? <LinearProgress mode='indeterminate'/> : null}
+        {busy ? this.renderProgress() : null}
       </div>
     )
   }
