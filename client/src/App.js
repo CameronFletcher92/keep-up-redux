838ba3ec4c1d13304277ad131c58e273e9c3727c
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-10-21 17:55:13 +1000

    Revert "deploy"
    This reverts commit 3a19fe5bb270ea5df723f5e8f5f3337388319ee9.

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,18 @@ import { ReduxRouter } from 'redux-router'
 import shouldUpdatePure from 'react-pure-render/function'
 import store from './store'
 import routes from './routes'
-// import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react'
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react'
 
 class App extends Component {
   shouldComponentUpdate = shouldUpdatePure
 
-  /*
   renderDevTools() {
     return (
       <DebugPanel top right bottom>
         <DevTools store={store} monitor={LogMonitor} />
       </DebugPanel>
-
     )
   }
-  */
 
   render() {
     /* global __DEV__ */
@@ -27,7 +24,7 @@ class App extends Component {
         <Provider store={store}>
           <ReduxRouter routes={routes} />
         </Provider>
-        {/* __DEV__ ? this.renderDevTools() : null */}
+        {__DEV__ ? this.renderDevTools() : null}
       </div>
     )
   }
