1b96e715a7cee55a4678c6fc26d1ef4b66ed6941
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-10-28 02:22:03 +1000

    hid annoying dialog for now

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,8 @@ injectTap()
 ReactDOM.render(<App />, document.getElementById('root'))
 
 /* global __DEV__ */
+/*
 if (!__DEV__) {
   window.onbeforeunload = () => 'Keep-Up will have to re-fetch data if you leave this page.'
 }
+*/
