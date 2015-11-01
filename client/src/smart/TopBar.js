9c9ababab3a5c1e1bef45bed1e6eb53b2d93efbe
Cameron Fletcher <cameronfletcher92@gmail.com>
2015-11-01 20:01:23 +1000

    added title to side nav

--- client/src/smart/TopBar.js
+++ client/src/smart/TopBar.js
@@ -5,10 +5,13 @@ import shouldUpdatePure from '../util/shouldUpdatePure'
 import { hideMessage } from '../ducks/global'
 import { AppBar, LeftNav, Snackbar } from '../themes/muiComponents'
 import { pushState } from 'redux-router'
+import { palette } from '../themes/muiTheme'
 
 const styles = {
   appbar: { position: 'fixed' },
   container: { display: 'flex', flexDirection: 'row', justifyContent: 'center', paddingTop: '4em' },
+  header: { background: palette.primary1Color, color: palette.alternateTextColor,
+            paddingTop: '0.7em', paddingBottom: '0.8em', paddingLeft: '1em', fontSize: '24px' },
   content: { display: 'flex', flex: '1', flexDirection: 'column', minWidth: '15em', maxWidth: '50em', margin: '0.5em' }
 }
 
@@ -28,6 +31,20 @@ class TopBar extends Component {
     }
   }
 
+  navHome() {
+    const props = this.props
+    props.pushState(null, '/')
+    this.refs.leftNav.close()
+  }
+
+  renderHeader() {
+    return (
+      <div style={styles.header} onClick={() => this.navHome()}>
+        Keep Up
+      </div>
+    )
+  }
+
   render() {
     const menuItems = [
       { route: '/clients', text: 'Clients' },
@@ -40,7 +57,7 @@ class TopBar extends Component {
     return (
       <div>
         <AppBar style={styles.appbar} title={props.title} zDepth={1} onLeftIconButtonTouchTap={() => this.refs.leftNav.toggle()}/>
-        <LeftNav ref='leftNav' menuItems={menuItems} docked={false}
+        <LeftNav ref='leftNav' menuItems={menuItems} docked={false} header={this.renderHeader()}
                  onChange={(ev, key, payload) => props.pushState({ title: payload.text }, payload.route)}/>
         <div style={styles.container}>
           <div style={styles.content}>
